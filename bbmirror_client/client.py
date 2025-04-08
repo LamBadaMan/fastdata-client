731d134f7e9be97728a03180de81a1b7eb8f25ba
Joel Val <joel.val@icloud.com>
2025-04-08 18:53:52 +0200

    return records

--- bbmirror_client/client.py
+++ bbmirror_client/client.py
@@ -8,10 +8,15 @@ HEADERS = {
 }
 
 def bdp(data):
-    return requests.post(f"{BASE_API_URL}/bdp/", headers=HEADERS, data=json.dumps(data))
+    response = requests.post(f"{BASE_API_URL}/bdp/", headers=HEADERS, data=json.dumps(data))
+    return json.loads(response.content.decode("utf-8"))
 
 def bdh(data):
-    return requests.post(f"{BASE_API_URL}/bdh/", headers=HEADERS, data=json.dumps(data))
+    response = requests.post(f"{BASE_API_URL}/bdh/", headers=HEADERS, data=json.dumps(data))
+    return json.loads(response.content.decode("utf-8"))
 
 def bds(data):
-    return requests.post(f"{BASE_API_URL}/bds/", headers=HEADERS, data=json.dumps(data))
+    response = requests.post(f"{BASE_API_URL}/bds/", headers=HEADERS, data=json.dumps(data))
+    return json.loads(response.content.decode("utf-8"))
+
+
